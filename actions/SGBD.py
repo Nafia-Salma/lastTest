1de2efe1ee7ef7efc80c2cc9354dda5caca51b9e
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-06-07 15:52:36 +0100

    update

--- actions/SGBD.py
+++ actions/SGBD.py
@@ -8,13 +8,10 @@ class mySGBD():
 
         self.allowed_categories = list(self.prd['category'].unique()) # list of the allowed categories
         
-        self.allowed_RAM = [] # list of the allowed colors for the chosen category
-        
-        self.allowed_sizes = [] # list of the allowed sizes for the chosen color
-        
-        self.allowed_prd = None # table of the allowed products for the chosen size
-        self.allowed_products = [] # list of the allowed products
-
+        self.allowed_RAM = [] 
+        self.allowed_processors = [] 
+        self.allowed_storage_capacities = []
+        self.allowed_brand_price = []
         self.allowed_quantity = 1 # product quantity in stock
     
     #get available RAMs for the chosen category
@@ -29,62 +26,41 @@ class mySGBD():
     def get_processor_by_RAM(self, RAM):
 
         self.allowed_prd = self.allowed_prd[self.allowed_prd['RAM'] == RAM]
-        self.allowed_processor = list(self.allowed_prd['processor'].unique())
+        self.allowed_processors = list(self.allowed_prd['processor'].unique())
         
-        return self.allowed_processor
+        return self.allowed_processors
     
      #get available processors for the chosen category
     def get_storage_capacity_by_processor(self, processor):
 
         self.allowed_prd = self.allowed_prd[self.allowed_prd['processor'] == processor]
-        self.allowed_storage_capacity = list(self.allowed_prd['storage_capacity'].unique())
+        self.allowed_storage_capacities = list(self.allowed_prd['storage_capacity'].unique())
         
-        return self.allowed_storage_capacity
+        return self.allowed_storage_capacities
 
      #get available processors for the chosen category
     def get_brand_by_storage_capacity(self, storage_capacity):
 
         self.allowed_prd = self.allowed_prd[self.allowed_prd['storage_capacity'] == storage_capacity]
-        self.allowed_brand = list(self.allowed_prd['brand', 'price'].unique())
+        self.allowed_brand_price = self.allowed_prd[['brand', 'price']].values.tolist()
         
-        return self.allowed_brand
+        return self.allowed_brand_price
     
-    # #get available processors for the chosen category
-    # def get_price_by_brand(self, brand):
+    def get_product_quantity_by_brand(self, brand):
 
-    #     self.allowed_prd = self.allowed_prd[self.allowed_prd['brand'] == brand]
-    #     self.allowed_price = list(self.allowed_prd['price'].unique())
-        
-    #     return self.allowed_price
+        self.allowed_prd = self.allowed_prd[self.allowed_prd['brand'] == brand]
+        self.allowed_quantity = self.allowed_prd['quantity'].tolist()[0]
+
+        return self.allowed_quantity
     
-    # #get available product's quantity for the chosen product name
-    # def get_product_quantity_by_size(self):
-
-    #     self.allowed_quantity = self.allowed_prd_s['size_quantity'].tolist()[0]
-
-    #     return self.allowed_quantity
-    
-    # #update the quantity of the ordered product
-    # def update_quantity(self, product, color, size, ordered_quantity):
-        
-    #     self.prd_s.loc[(self.prd_s['product_id'] == product.id) & (self.prd_s['color_id'] == color.id) & (self.prd_s['size'].str.lower() == size.lower()), 'size_quantity'] -= ordered_quantity
-
-        
-    #     self.prd_s = self.prd_s[self.prd_s['size_quantity'] > 0]
-
-        
-    #     self.prd_c.loc[(self.prd_c['product_id'] == product.id) & (self.prd_c['id'] == color.id), 'color_quantity'] -= ordered_quantity
-
-        
-    #     self.prd_c = self.prd_c[self.prd_c['color_quantity'] > 0]
-
+    def update_quantity(self, ordered_quantity):
+        product_info = self.allowed_prd.values.tolist()
+        product_id = product_info[0][0]
          
-    #     self.prd.loc[self.prd['id'] == product.id, 'quantity'] -= ordered_quantity
+        self.prd.loc[self.prd['id'] == product_id, 'quantity'] -= ordered_quantity
+        print(self.prd[self.prd['id'] == product_id])
 
-        
-    #     self.prd = self.prd[self.prd['quantity'] > 0]
-
-
+        self.prd = self.prd[self.prd['quantity'] > 0]
 
     
 if __name__ == '__main__':
@@ -95,5 +71,20 @@ if __name__ == '__main__':
     print(categories)
 
     RAM = sgbd.get_RAM_by_category('Laptop')
-    print(sgbd.allowed_prd)
     print(RAM)
+
+    processors = sgbd.get_processor_by_RAM('32GB')
+    print(processors)
+
+    capacities = sgbd.get_storage_capacity_by_processor('Intel Core i9')
+    print(capacities)
+
+    brands = sgbd.get_brand_by_storage_capacity('1TB SSD')
+    print(brands)
+
+    quantity = sgbd.get_product_quantity_by_brand('HP')
+    print(sgbd.allowed_prd)
+    print(quantity)
+
+    sgbd.update_quantity(1)
+
